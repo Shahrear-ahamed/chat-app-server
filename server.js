077e71f9aeee43263b80e04fb4c9c8530cc53106
Shahrear Ahamed <its.shahrear@gmail.com>
2022-09-16 23:59:17 +0600

    try to fix cors issues

--- server.js
+++ server.js
@@ -9,7 +9,17 @@ const server = http.createServer(app);
 const io = require("socket.io")(server);
 
 global.io = io;
-app.use(cors())
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    );
+    next();
+  });
 
 const router = jsonServer.router("db.json");
 
